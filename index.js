bbf435e4bad534aff518fa2ff2ae16b923d86c79
vincent <omegaortiz1997@gmail.com>
2018-03-28 17:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function johnLennonFacts(facts) {
   return array;
 }
 
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return array;
+}
